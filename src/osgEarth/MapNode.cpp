0634b1d614c3c5e3fb34ff32cd5953a625490c37
Glenn Waldron <gwaldron@gmail.com>
2021-01-20 09:57:09 -0500

    Update the TerrainEngine API to expose methods through its interface instead of through a group node

--- src/osgEarth/MapNode.cpp
+++ src/osgEarth/MapNode.cpp
@@ -558,7 +558,7 @@ MapNode::computeBound() const
     osg::BoundingSphere bs;
     if ( getTerrainEngine() )
     {
-        bs.expandBy( getTerrainEngine()->getBound() );
+        bs.expandBy( getTerrainEngine()->getNode()->getBound() );
     }
 
     if (_layerNodes)
@@ -617,7 +617,7 @@ MapNode::getTerrain() const
     return getTerrainEngine()->getTerrain();
 }
 
-TerrainEngineNode*
+TerrainEngine*
 MapNode::getTerrainEngine() const
 {
     return _terrainEngine;
