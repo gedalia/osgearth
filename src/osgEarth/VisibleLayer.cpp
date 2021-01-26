2c3d8f61a571a89548e7452630d09a1c2cd6ea1f
Glenn Waldron <gwaldron@gmail.com>
2021-01-26 14:00:08 -0500

    VisibleLayer: decouple setMask from setVisible

--- src/osgEarth/VisibleLayer.cpp
+++ src/osgEarth/VisibleLayer.cpp
@@ -123,9 +123,14 @@ VisibleLayer::openImplementation()
     if (parent.isError())
         return parent;
 
-    if (options().visible().isSet() || options().mask().isSet() )
+    if (options().visible().isSet())
     {
         setVisible(options().visible().get());
+    }
+
+    if (options().mask().isSet())
+    {
+        setMask(options().mask().get());
     }
 
     initializeBlending();
@@ -147,9 +152,6 @@ VisibleLayer::setVisible(bool value)
     osg::Node* node = getNode();
     if (node)
     {
-        // Still set the node mask
-        node->setNodeMask(getMask());
-
         if (value == true)
         {
             if (_noDrawCallback.valid())
@@ -184,7 +186,10 @@ VisibleLayer::setMask(osg::Node::NodeMask mask)
     options().mask() = mask;
 
     // Call setVisible to make sure the mask gets applied to a node if necessary
-    setVisible(options().visible().get());;
+    if (getNode())
+    {
+        getNode()->setNodeMask(mask);
+    }
 }
 
 osg::Node::NodeMask
